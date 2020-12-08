Fixed range sensor callback and code cleanup. weighting function should work, resampling still WIP

localization/nodes/localization.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 import math
 from std_msgs.msg import Float64
-from geometry_msgs.msg import Pose, Point
+from geometry_msgs.msg import PoseStamped, Point
 from range_sensor.msg import RangeMeasurementArray, RangeMeasurement
 
 
@@ -26,9 +26,10 @@ class LocalizationNode():
 
         self.num_particles = PARTICLE_COUNT
         self.particles = np.array([[0,0,0]])
-
-        self.ranges = []
+# make array
         self.weights = []
+
+        self.ranges = np.array([0.0,0.0,0.0,0.0])
 
         self.roll = 0
         self.pitch = 0
@@ -37,43 +38,27 @@ class LocalizationNode():
         self.initialize_particles()
 
         self.position_pub = rospy.Publisher("estimated_position", Point, queue_size=1)
-        self.yaw_pub = rospy.Publisher("yaw", Float64, queue_size=1)
+        #self.yaw_pub = rospy.Publisher("yaw", Float64, queue_size=1)
+        #self.particle_count_pub = rospy.Publisher("particle_count", Float64, queue_size=1)
         
         self.range_sub = rospy.Subscriber("ranges", RangeMeasurementArray, self.range_callback, queue_size=1)
-        self.orientation_sub = rospy.Subscriber("mavros/local_position/pose", Pose, self.orientation_callback, queue_size=1)
+        self.orientation_sub = rospy.Subscriber("mavros/local_position/pose", PoseStamped, self.orientation_callback, queue_size=1)
     
 
     def range_callback(self, msg):
-# Get data correctly
-        self.ranges = msg.measurements
-        # self.ranges[2] = msg.
-        # self.ranges[3] = msg.
-        # self.ranges[4] = msg.
-        message = Float64()
-        message.data = ranges[0]
-        self.yaw_pub.publish(message)
-
-        # valid_sensors = 4
-        # for i in self.range:
-        #     if self.range[i] == 0:
-        #         valid_sensors -= 1
-        
-        # if valid_sensors < 3:
-        #     pass
-
-        # tag nummerierung ist bekannt! -> checke ob links oder rechts rum drehen
-
-        #Turn till three sensor measurements are valid
+        for measurement in msg.measurements:
+            self.ranges[measurement.id-1] = measurement.range
 
 
     def orientation_callback(self, msg):
-        x = msg.orientation.x
-        y = msg.orientation.y
-        z = msg.orientation.z
-        w = msg.orientation.w
-        self.roll = math.atan2(2*y*w - 2*x*z, 1 - 2*y*y - 2*z*z)
-        self.pitch = math.atan2(2*x*w - 2*y*z, 1 - 2*x*x - 2*z*z)
-        self.yaw = math.asin(2*x*y + 2*z*w)        
+        pass
+        # x = msg.orientation.x
+        # y = msg.orientation.y
+        # z = msg.orientation.z
+        # w = msg.orientation.w
+        # self.roll = math.atan2(2*y*w - 2*x*z, 1 - 2*y*y - 2*z*z)
+        # self.pitch = math.atan2(2*x*w - 2*y*z, 1 - 2*x*x - 2*z*z)
+        # self.yaw = math.asin(2*x*y + 2*z*w)        
 
 
     def euclidean_dist(self, point1, point2):
@@ -87,17 +72,18 @@ class LocalizationNode():
             z = round(random.random(), 4) * TANK_HEIGHT
             self.particles = np.append(self.particles, [[x, y, z]], axis=0)
 
-            # self.particles[n] returns nth particle
-            # self.particles[n][m] returns mth coordinate of nth particle
+            # self.particles[n] returns n'th particle
+            # self.particles[n][m] returns m'th coordinate of nth particle
 
 
     def dead_reckon(self):
         pass
-        #addiere rauschen auf bewegung
-        # nutze imu daten
+# Use IMU Data
+# Add Gaussian Noise
 
 
     def weighting(self):
+        weights = []
         for i in range(self.num_particles):
             diff_1 = self.euclidean_dist(self.particles[i], self.tag_1) - self.ranges[0]
             diff_2 = self.euclidean_dist(self.particles[i], self.tag_2) - self.ranges[1]
@@ -105,41 +91,43 @@ class LocalizationNode():
             diff_4 = self.euclidean_dist(self.particles[i], self.tag_4) - self.ranges[3]
 
             mean_diff = (diff_1 + diff_2 + diff_3 + diff_4) / 4
-            self.weights.append(mean_diff)
+            weights.append(mean_diff)
+        self.weights = weights
 
         # Normalize weights
         self.weights /= np.sum(self.weights)
 
 
-    def resample(self, weights):
+    def resample(self):
         particles = []
+        #particles = np.array([[0,0,0]])
 
-        # Normalize weights
-        norm_weights = weights/np.sum(weights)
         
-        indices = [np.random.choice(np.arange(0, self.num_particles), p=norm_weights) for i in range(self.num_particles)]
+        #norm_weights = weights/np.sum(weights)
+        
+        indices = [np.random.choice(np.arange(0, self.num_particles), p=self.weights) for i in range(self.num_particles)]
 
         for i in indices:
-            particles.append(self.particles[i])
+            particles.append(self.list_test[i])
+            #particles = np.append(particles, self.particles[i])
         assert self.num_particles == len(particles)
 
         self.particles = particles
         self.num_particles = len(particles)
 
-        msg = Float64()
-        msg.data = num_particles
-        self.particle_count_pub.publish(msg)
+        # msg = Float64()
+        # msg.data = num_particles
+        # self.particle_count_pub.publish(msg)
 
 
     def run(self):
         rate = rospy.Rate(100.0)
         while not rospy.is_shutdown():
             
-            self.weighting()
-            
-            self.resample()
+            self.weighting() 
+            #self.resample()
 
-                
+
 
 
             msg = Point()
@@ -156,7 +144,7 @@ class LocalizationNode():
             msg.y = mean_pos_y / self.num_particles
             msg.z = mean_pos_z / self.num_particles
     
-            self.point_pub.publish(msg)
+            self.position_pub.publish(msg)
 
 
             rate.sleep()

localization/nodes/minimizing_cost_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-import scipy
+from scipy.optimize import minimize
 import random
 import math
 from std_msgs.msg import Float64
@@ -21,24 +21,31 @@ class MinimizationNode():
         self.bounds = [(0,2),(0,3.35),(-1.2,0)]
         self.x0 = np.array([0.8,1.5,-0.7])
 
+        self.measurements = np.zeros((4, 2))
+
         self.range_sub = rospy.Subscriber("ranges", RangeMeasurementArray, self.range_callback, queue_size=1)
         self.position_pub = rospy.Publisher("raw_position", Point, queue_size=1)    
     
-    def range_callback(self, msg):
-        self.distances = msg.range    
+    def range_callback(self, range_array_msg):
+        number_anchors = len(range_array_msg.measurements) 
+        for index, measurement in enumerate(range_array_msg.measurements):
+            anchor_id = measurement.id
+            range_measurement = measurement.range        
+            self.measurements[index, 0] = range_measurement
+            self.measurements[index, 1] = anchor_id    
         
     def costfun(self, x):
-        y1 = np.linalg.norm(tag1-x)
-        y2 = np.linalg.norm(tag2-x)
-        y3 = np.linalg.norm(tag3-x)
-        y4 = np.linalg.norm(tag4-x)
+        y1 = np.linalg.norm(self.tag1-x)
+        y2 = np.linalg.norm(self.tag2-x)
+        y3 = np.linalg.norm(self.tag3-x)
+        y4 = np.linalg.norm(self.tag4-x)
         y = np.array([y1, y2, y3, y4])
         return np.mean((y-self.distances)**2)    
         
     def run(self):
         rate = rospy.Rate(100.0)
         while not rospy.is_shutdown():
-            x = scipy.optimize.minimize(costfun, self.x0, args=(self), method="TNC", bounds=self.bounds)
+            x = minimize(self.costfun, self.x0, args=(self), method="TNC", bounds=self.bounds)
             msg = Point()
             msg.data = x
             self.position_pub.publish(msg)